9ce1fefa56a66a5802cac00c3eb32793678b3e3d
User <yakontroller@gmail.com>
2021-03-06 01:07:15 +0300

    promotion with picture

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "/promotion_image"
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
