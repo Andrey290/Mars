2ce7c3ad3ef71afc84116f5ca6953c1d4e36df39
User <yakontroller@gmail.com>
2021-03-10 21:44:57 +0300

    не работает

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ def index():
                     <p>
                         /choice/
                     </p>
+                    <p>
+                        /results/<nickname>/<int:level>/<float:rating>
+                    </p>
                 </body>'''
 
 
@@ -58,7 +61,36 @@ def choice(planet_name):
 
 @app.route('/results/<nickname>/<int:level>/<float:rating>')
 def results(nickname, level, rating):
-    return
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Варианты выбора</title>
+                  </head>
+                  <body>
+                        <h1>Результаты отборочного отбора</h1>
+                        <h2>Претендента на участие в миссии {nickname}<h2>
+
+                        <div class="alert alert-success" role="alert">
+                            Поздравляем! Ваш рейтинг после {level} эиапа отбора
+                        </div>
+
+                        <div>
+                            Составляет {rating}
+                        </div>
+
+                        <div class="alert alert-warning" role="alert">
+                            Желаем удачи!
+                        </div>
+                  </body>
+                </html>
+'''
+
 
 @app.route('/promotion_image')
 def promotion_image():
