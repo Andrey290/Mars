a075af15ad5982a960be212cc6cba94394b1683c
User <yakontroller@gmail.com>
2021-03-11 00:54:12 +0300

    есть вопрос

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -205,12 +205,42 @@ def form_sample():
         print("Фамилия", request.form['family_name'])
         print("Имя", request.form['name'])
         print("Статус образованности", request.form['education'])
-
         print("Почему", request.form['why'])
         print("Пол", request.form['gender'])
         print("Согласен", request.form['accept'])
         return "Форма отправлена"
 
+@app.route('/load_photo')
+def load_photo():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                             <link rel="stylesheet"
+                             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                             crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Пример загрузки файла</title>
+                          </head>
+                          <body>
+                            <h1>Загрузим файл</h1>
+                            <form method="post" enctype="multipart/form-data">
+                               <div class="form-group">
+                                    <label for="photo">Выберите файл</label>
+                                    <input type="file" class="form-control-file" id="photo" name="file">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Отправить</button>
+                            </form>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        f = request.files['file']
+        print(f.read())
+        # как на этом моменте сохранить изображение?
+        return "Форма отправлена"
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
