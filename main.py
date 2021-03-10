008cfed0b2481d1359fef29768fab14e868ab586
User <yakontroller@gmail.com>
2021-03-11 00:47:31 +0300

    фотка не загружается

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
+import os
+import sys
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -81,7 +84,7 @@ def results(nickname, level, rating):
                         </div>
 
                         <div>
-                            Составляет {rating}
+                            Составляет {rating}!
                         </div>
 
                         <div class="alert alert-warning" role="alert">
@@ -100,16 +103,119 @@ def promotion_image():
 @app.route('/form_sample', methods=['POST', 'GET'])
 def form_sample():
     if request.method == 'GET':
-        return render_template('form_base.html')
+        return f'''
+                <!doctype html>
+                <html lang="en">
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet"
+                          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                          crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="static/css/form_style.css"/>
+                    <title>Пример формы</title>
+                </head>
+                <body>
+                
+                <h2>Форма заявки на участие в миссии</h2>
+                
+                <div>
+                    <form class="login_form" method="post">
+                
+                        <input type="family_name" class="form-control" id="family_name" aria-describedby="emailHelp"
+                               placeholder="Введите фамилию" name="family_name">
+                
+                        <input type="name" class="form-control" id="name" placeholder="Введите имя" name="name">
+                
+                        <div class="form-group">
+                            <label for="classSelect">Какое Ваше образование</label>
+                            <select class="form-control" id="education" name="education">
+                                <option>ученик Яндекс.Лицей</option>
+                                <option>Школьник</option>
+                                <option>Студент коледжа</option>
+                                <option>Бакалавр</option>
+                                <option>Специалитет</option>
+                                <option>Магистр</option>
+                                <option>Аспирант</option>
+                                <option>Просветлённый</option>
+                            </select>
+                        </div>
+                
+                        <div>
+                            <label>
+                                Какова Ваша профессия
+                            </label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" name="occupationPROG>
+                                                            <label class=" form-check-label" for="flexCheckDefault">
+                                Программист
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value=""  name="occupationROBO>
+                                                          <label class=" form-check-label" for="flexCheckChecked">
+                                Робототехник
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value=""  name="occupationCHINA">
+                                <label class="form-check-label" for="flexCheckChecked">
+                                    Переводчик с китайского(中文翻译)
+                                </label>
+                            </div>
+                        </div>
+                
+                        <div class="form-group">
+                            <label for="why">Почему?</label>
+                            <textarea class="form-control" id="why" rows="3" name="why"></textarea>
+                        </div>
+
+                        <div method="post" enctype="multipart/form-data">
+                            <div class="form-group">
+                                <label for="photo">Выберите файл</label>
+                                <input type="file" class="form-control-file" id="photo" name="file">
+                            </div>
+                            <img src="{url_for('static', filename='img/image.jpg')}" alt="">
+                        </div>
+                
+                        <div class="form-group">
+                            <label for="form-check">Укажите пол</label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gender" id="male" value="male" checked>
+                                <label class="form-check-label" for="male">
+                                    Мужской
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gender" id="female" value="female">
+                                <label class="form-check-label" for="female">
+                                    Женский
+                                </label>
+                            </div>
+                        </div>
+                
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
+                            <label class="form-check-label" for="acceptRules">Соглашаться!</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Отправить</button>
+                    </form>
+                </div>
+                </body>
+                </html>'''
     elif request.method == 'POST':
-        print(request.form['family_name'])
-        print(request.form['name'])
-        print(request.form['email'])
-        print(request.form['education'])
-        print(request.form['occupation'])
-        print(request.form['gender'])
-        print(request.form['why'])
-        print(request.form['accept'])
+        print("Фамилия", request.form['family_name'])
+        print("Имя", request.form['name'])
+        print("Статус образованности", request.form['education'])
+
+        f = request.files['file'] # тут я получаю файл
+        a = f.read() # тут я его читаю
+        a.save("static/img/image.jpg") # тут сохраняю
+
+        print("Почему", request.form['why'])
+        print("Пол", request.form['gender'])
+        print("Согласен", request.form['accept'])
         return "Форма отправлена"
 
 
